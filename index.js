70c5939951f964907b2da288c2c93e57ec260e5d
FlasonNguyen <30571019+FlasonNguyen@users.noreply.github.com>
2022-03-21 12:16:36 +0700

    update

--- index.js
+++ index.js
@@ -1,55 +1,60 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const session = require('express-session')
-const passport = require('passport')
-const app = express()
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const app = express();
+require("dotenv").config();
 
-app.set('view engine', 'ejs')
-app.use(express.static(__dirname + '/public'))
-app.use('/product',express.static(__dirname + '/public'))
-app.use('/product/brand',express.static(__dirname + '/public'))
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use("/product", express.static(__dirname + "/public"));
+app.use("/product/brand", express.static(__dirname + "/public"));
 
-app.use('/product/filter',express.static(__dirname + '/public'))
-app.use('/product/filter/male',express.static(__dirname + '/public'))
+app.use("/product/filter", express.static(__dirname + "/public"));
+app.use("/product/filter/male", express.static(__dirname + "/public"));
 
-app.use('/product/filter/female',express.static(__dirname + '/public'))
-app.use('/account',express.static(__dirname + '/public'))
-app.use('/cart',express.static(__dirname + '/public'))
-app.use('/admin',express.static(__dirname + '/public'))
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use("/product/filter/female", express.static(__dirname + "/public"));
+app.use("/account", express.static(__dirname + "/public"));
+app.use("/cart", express.static(__dirname + "/public"));
+app.use("/admin", express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.use(session({
-    secret: 'khoa',
+app.use(
+  session({
+    secret: "khoa",
     resave: false,
     saveUninitialized: true,
-    cookie: {secret: false}
-}))
+    cookie: { secret: false },
+  })
+);
 
 //-------------------------GOOGLE LOGIN CONFIG------------------------------
 
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
-require('./oauth')
+require("./oauth");
 
-app.use('/oauth/google', require('./route/google'))
+app.use("/oauth/google", require("./route/google"));
 
 //-------------------------GOOGLE LOGIN CONFIG------------------------------
 
-app.get('/', (req, res) => {
-    res.redirect('dashboard')
-})
-app.use('/dashboard', require('./route/dashboard'))
-app.use('/product', require('./route/product'))
-app.use('/account',require('./route/account'))
-app.use('/admin',require('./route/admin'))
-app.use('/cart',require('./route/cart'))
+app.get("/", (req, res) => {
+  res.redirect("dashboard");
+});
+app.use("/dashboard", require("./route/dashboard"));
+app.use("/product", require("./route/product"));
+app.use("/account", require("./route/account"));
+app.use("/admin", require("./route/admin"));
+app.use("/cart", require("./route/cart"));
 
-mongoose.connect('mongodb://localhost:27017/myapp')
-    .then(() => {
-        console.log('Connected')
-        app.listen(process.env.PORT, () => {console.log(`http://localhost:${process.env.PORT}`)})
-    })
-    .catch(e => console.log('Can\'t connect to database '+ e.messages))
+mongoose
+  .connect(process.env.MONGODB_CONNECTION)
+  .then(() => {
+    console.log("Connected");
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((e) => console.log("Can't connect to database " + e.messages));
